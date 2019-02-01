d5da7e84faf4c91183de6415a8622f192dce39aa
XhantiGhost <xhantijacob@gmail.com>
2019-02-01 16:11:44 +0200

    just

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 //connect to database
 include_once 'connect.php';
 
@@ -22,4 +21,26 @@ $sql = "CREATE TABLE film (
     ?>
 
 
-?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+
+    <form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="Mickey"><br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse"><br>
+  Email:<br>
+  <input type="text" name="email" placeholder="email"><br>
+  <input type="submit" value="Submit">
+  <input type="reset">
+</form>
+        
+    </body>
+    </html>
